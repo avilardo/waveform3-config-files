34278b30eac128f532aea678b8b6a9c2e89ece6b
Chris Jurado <unixknight@yahoo.com>
2014-02-07 11:27:47 -0500

    Update setenv.sh

--- waveform3/tomcat/bin/setenv.sh
+++ waveform3/tomcat/bin/setenv.sh
@@ -1,3 +1,3 @@
-JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=256m"
+JAVA_OPTS="$JAVA_OPTS -Dfile.encoding=UTF8 -Dorg.apache.catalina.loader.WebappClassLoader.ENABLE_CLEAR_REFERENCES=false -Duser.timezone=GMT -Xmx1024m -XX:MaxPermSize=1024m"
 LD_LIBRARY_PATH="/opt/wfdb/lib64"
 PATH="$PATH:/opt/wfdb/bin"
